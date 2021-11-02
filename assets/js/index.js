27e91862926425546f8078fa44840ca442679bc3
StormZZ001 <976183601@qq.com>
2021-11-02 21:23:15 +0800

    2.11实现退出功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,3 +40,19 @@ function renderAvatar(user){
         $('.text-avatar').html(text).show()
     }
 }
+//实现退出功能
+var layer = layui.layer
+//点击按钮,实现退出功能
+$('#btnLogout').on('click',function () { 
+    //提示用户是否确认退出
+    layer.confirm('确定退出登录?',{icon:3,title:'提示'},function (index) { 
+        //do something
+        //1.清空本地存储中的token
+        localStorage.removeItem('token')
+        //2.重新跳转到登录页面
+        location.href = 'login.html'
+
+        //关闭confirm询问框
+        layer.close(index)
+     })
+ })
