8e24b988bfc17992932754c6fa57d14134c85a61
StormZZ001 <976183601@qq.com>
2021-11-02 21:09:52 +0800

    2.10统一为有权限的接口设置headers请求头

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,8 +3,14 @@
  * 会先调用 ajaxPrefilter 这个函数
  * 在这个函数中,可以拿到我们给 Ajax 提供的配置对象
  */
-$.ajaxPrefilter(function (options) { 
+$.ajaxPrefilter(function (options) {
     //在发起真正的 Ajax 请求之前,统一拼接请求的根路径
-
     options.url = 'http://www.liulongbin.top:3008' + options.url
- })
+    // 统一为有权限的接口，设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+})
