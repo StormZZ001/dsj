bf50b8f2f67cb27949a1a10580402d7094238b62
StormZZ001 <976183601@qq.com>
2021-11-11 19:25:13 +0800

    5.4更换头像 - -实现裁剪区域图片的替换

--- user/js/user_avatar.js
+++ user/js/user_avatar.js
@@ -7,11 +7,28 @@ const options = {
   // 指定预览区域
   preview: '.img-preview'
 }
-
 // 1.3 创建裁剪区域
 $image.cropper(options)
-
+let layer = layui.layer
 //为上传按钮绑定点击事件
-$('#btnUpload').on('click',function(){
+$('#btnUpload').on('click', function () {
   $('#file').click()
+})
+// 为文件选择框绑定 change 事件
+$('#file').on('change', function (e) {
+  // 获取用户选择的文件
+  var filelist = e.target.files
+  console.log(filelist)
+  if (filelist.length === 0) {
+    return layui.layer.msg('请选择照片！')
+  }
+  //1.拿到用户选择的文件
+  let file = e.target.files[0]
+  //2.将文件，转化为路径
+  var imgURL = URL.createObjectURL(file)
+  //3.重新初始化裁剪区域
+  $image
+    .cropper('destroy')//销毁旧的裁剪区域
+    .attr('src',imgURL) //重新设置图片路径
+    .cropper(options)//重新初始化裁剪区域
 })
